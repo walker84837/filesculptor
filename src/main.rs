1bd8c93336055a67cdf1d37ca9f2efabdb92064d
#84837 <walker84837@gmail.com>
2024-03-18 21:14:21 +0100

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,21 @@
 use anyhow::{anyhow, Result};
 use clap::Parser;
 use serde_derive::Deserialize;
-use std::{
-    collections::HashMap,
-    fs::{self, File},
-    io::{BufReader, BufWriter, Read, Write},
-    path::PathBuf,
-};
+use std::{collections::HashMap, fs, path::PathBuf};
 
 #[derive(Parser)]
 struct Args {
-    #[arg(short, long = "input", help = "File to normalize")]
+    /// File to normalize
+    #[arg(short, long = "input")]
     input_file: PathBuf,
 
-    #[arg(short, long = "output", help = "Output file")]
+    /// Output file
+    #[arg(short, long = "output")]
     output_file: PathBuf,
 
-    #[arg(
-        short,
-        long = "config",
-        help = "JSON configuration file",
-        default_value = "config.json"
-    )]
+    /// JSON configuration file
+    #[arg(long = "config", default_value = "config.json")]
     config_path: PathBuf,
-}
-
-struct FileOperations;
-
-impl FileOperations {
-    pub fn read_file(path: String) -> Result<String> {
-        let file = File::open(path)?;
-
-        let mut reader = BufReader::new(file);
-        let mut contents = String::new();
-        reader.read_to_string(&mut contents)?;
-        
-        Ok(contents)
-    }
-
-    pub fn write(path: String, contents: &[u8]) -> Result<()> {
-        let file = File::create(path)?;
-
-        let mut reader = BufWriter::new(file);
-        reader.write_all(contents)?;
-        Ok(())
-    }
 }
 
 #[derive(Deserialize)]
@@ -60,28 +31,20 @@ pub fn normalize(text: &str, changes: &HashMap<String, String>) -> String {
         })
 }
 
-fn load_config(config_path: &PathBuf) -> Result<Config> {
-    let config_content = fs::read_to_string(config_path)?;
-    let contents = serde_json::from_str(&config_content)?;
-    Ok(contents)
-}
-
 fn main() -> Result<()> {
     let args = Args::parse();
-    let pathbuf_to_string = |path: PathBuf| -> String {
-        path.to_string_lossy().into_owned()
-    };
 
     if !args.input_file.exists() {
         return Err(anyhow!("Input file doesn't exist. Quitting..."));
     }
 
-    let contents = FileOperations::read_file(pathbuf_to_string(args.input_file))?;
+    let contents = fs::read_to_string(args.input_file)?;
+    let config = fs::read_to_string(args.config_path)?;
 
-    let config = load_config(&args.config_path)?;
-    let result = normalize(contents.as_str(), &config.changes);
+    let tmp: Config = serde_json::from_str(&config)?;
 
-    FileOperations::write(pathbuf_to_string(args.output_file), &result.as_bytes())?;
+    let result = normalize(contents.as_str(), &tmp.changes);
+    fs::write(&args.output_file, &result)?;
 
     if result != contents {
         println!(
@@ -89,7 +52,7 @@ fn main() -> Result<()> {
             result.len()
         );
     } else {
-        println!("Modifications were applied, but nothing changed.");
+        println!("INFO: Modifications were applied, but nothing changed.");
     }
 
     Ok(())
